14f4668e664c68a1ae4402121fa1fbb552a6bd69
Thomas Schmitz <thomas.schmitz80@gmail.com>
2019-11-18 21:19:06 +0100

    Refactor code to fix problem with folder selected
    Overall the plugin works now more stable.

--- mql5_compiler.py
+++ mql5_compiler.py
@@ -9,39 +9,31 @@ EXTENSION  = ['.mq5','mq4']
 
 class Mql5CompilerCommand(sublime_plugin.TextCommand):
 
-    settings = sublime.load_settings("Mql5Compiler.sublime-settings")
-    log_file = tempfile.gettempdir() + '/compile.log'
-    metaeditor = "wrong"
-
     def init(self):
         view = self.view
+        self.is_error = True
         self.settings = sublime.load_settings("Mql5Compiler.sublime-settings")
-        self.metaeditor = self.settings.get("metaeditor_file")
         if view.file_name() is not None :
+            self.metaeditor = self.settings.get("metaeditor_file")
+            self.log_file = tempfile.gettempdir() + '/compile.log'
+            self.mql_home = self.settings.get("mql5_home")
+            self.inc_path = expandvars(self.mql_home)
             self.dirname   = os.path.realpath(os.path.dirname(view.file_name()))
             self.filename  = os.path.basename(view.file_name())
             self.extension = os.path.splitext(self.filename)[1]
-
-    def isError(self):
-        iserror = False
-        if not os.path.exists(self.metaeditor):
-            print ("Mql5 Compiler | error: metaeditor64.exe not found")
-            iserror = True
-
-        else :
+            self.is_error = False
+            if not os.path.exists(self.metaeditor):
+                print("Mql5Compiler - metaeditor64.exe not found:" + self.metaeditor)
+                self.is_error = True
             if self.extension not in EXTENSION:
-                print ("Mql5 Compiler | error: Wrong file extension: ({0})".format(self.extension))
-                iserror = True
-
+                print("Mql5Compiler - Wrong file extension: ({0})".format(self.extension))
+                self.is_error = True
             if self.view.is_dirty():
-                print ("Mql5 Compiler | error: Save File before compiling")
-                iserror = True
-                
-        return iserror
+                print("Mql5Compiler - Save file before compiling")
+                self.is_error = True
 
     def compile(self):
-        inc_path = expandvars(self.settings.get("mql5_home"))
-        proc = subprocess.Popen([self.metaeditor,"/compile:"+self.filename,"/log:"+self.log_file,"/inc:"+inc_path],
+        proc = subprocess.Popen([self.metaeditor,"/compile:"+self.filename,"/log:"+self.log_file,"/inc:"+self.inc_path],
         cwd= self.dirname,
         stdout=subprocess.PIPE,
         shell=False,
@@ -64,8 +56,6 @@ class Mql5CompilerCommand(sublime_plugin.TextCommand):
 
     def run(self , edit):
         self.init()
-        if self.isError():
-            return
-
-        self.compile()
-        self.showlog_file()
+        if not self.is_error:
+            self.compile()
+            self.showlog_file()
